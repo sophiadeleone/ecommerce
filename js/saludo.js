575003f872132fb602fbc620655685226245a215
Victoria Artusi <vikyartusi@hotmail.com>
2024-06-14 12:05:37 -0300

    login b  listo

--- js/saludo.js
+++ js/saludo.js
@@ -1,7 +1,20 @@
-let recstorage_saludo = localStorage.getItem(mail)
+let recstorage_saludo = localStorage.getItem('mail')
 
 if (recstorage_saludo !== null){
-    console.log("hay mail guardado")
+    console.log(recstorage_saludo)
+    let headerMail = document.querySelector('.nav-hom');
+    headerMail.innerHTML += `<li> Bienvenido: ${recstorage_saludo} </li>
+                            <li> <a href="index.html">LOGOUT</a> </li>`
+
+    let register = document.querySelector('.nav_reg');
+    register.style.display = 'none';
+
+    let login = document.querySelector('.nav_login');
+    login.style.display = 'none';
+
+
+
+//AGREGAR li
 
 }else{
     console.log("no hay mail")
