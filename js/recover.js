b6e23f3d5e28af1a2cd39d5f75f5fcb78f598412
sophiadeleone <sdeleone@udesa.edu.ar>
2024-06-20 09:05:53 -0930

    ediciones js recover

--- js/recover.js
+++ js/recover.js
@@ -4,8 +4,6 @@ let checkRec = document.querySelector("#checkRec");
 let todoOkInsturc= document.querySelector(".todoOkInsturc");
 let linkLog= document.querySelector(".linkLog");
 
-
-
 //no quiero que tenga mensajes en el div de errores previos, asi que al principio le digo que sea un str vacio
 document.querySelector(".emailErRec").innerText = "";
 document.querySelector(".checkErRec").innerText = "";
@@ -15,15 +13,15 @@ document.querySelector(".linkLog").innerHTML = ""; //PREGUNTAR lo dejo como str 
 formulario.addEventListener("submit", function(e) {
     e.preventDefault(); //detnego comportamiento defaut de form 
     
-    let mailRec = email.value; //aka lo que el usurario completo en el campo de email
+    let mailRec = emailRec.value; //aka lo que el usurario completo en el campo de email
 
     if (mailRec===""){
         document.querySelector(".emailErRec").innerText = "Por favor escriba su email.";
-    }else if (!checkRec){ //aca estoy diciendo si el checkboox no esta marcsado (por que checkbox es dato booleando que da true si esta marrcado )
+    }else if (!checkRec.checked){ //aca estoy diciendo si el checkboox no esta marcsado
         document.querySelector(".checkErRec").innerText = "Por favor acepte el campo Quiero recuperar mi contraseña";
     }else {
-        document.querySelector(".todoOkInsturc").innerText += "Recibirás un email con las instrucciones para recuperar tu contraseña";
+        todoOkInsturc.innerText = "Recibirás un email con las instrucciones para recuperar tu contraseña";
+        linkLog.innerHTML = '<a href="./login.html">Ir al login.</a>';
         formulario.submit();
-        document.querySelector(".linkLog").innerHTML += <a href="login.js" >Ir al login.</a>;
     }
 });
