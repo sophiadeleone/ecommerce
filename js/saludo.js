57092aa48befa4ae684b4652b663dce6803319eb
Victoria Artusi <vikyartusi@hotmail.com>
2024-06-20 12:16:23 -0300

    css saludo.js y arreglo de tamanio imagenes

--- js/saludo.js
+++ js/saludo.js
@@ -3,7 +3,7 @@ let recstorage_saludo = localStorage.getItem('mail')
 if (recstorage_saludo !== null){
     console.log(recstorage_saludo)
     let headerMail = document.querySelector('.nav-hom');
-    headerMail.innerHTML += `<li> Bienvenido: ${recstorage_saludo} </li>
+    headerMail.innerHTML += `<li class="saludo"> Bienvenido: ${recstorage_saludo} </li>
                             <li> <a href="index.html">LOGOUT</a> </li>`
 
     let register = document.querySelector('.nav_reg');
